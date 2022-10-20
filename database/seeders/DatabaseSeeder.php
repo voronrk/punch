fcfb07e9fa2f15c59a0be82c7f90e215a6c9e395
voron_76 <voron_76@mail.ru>
2022-10-20 13:53:16 +0300

    MSC for machines, materials, products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(MachineSeeder::class);
+        $this->call(MaterialSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
